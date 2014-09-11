b87f8ec8070e8c44450bf0c4484373fdc6880652
Aleksi Kaistinen <aleksi.kaistinen@gmail.com>
2014-09-11 14:15:45 +0300

    initial commit

---
+++ src/js/game.js
@@ -0,0 +1,163 @@
+(function() {
+	'use strict';
+
+	function Game() {
+		this.player = null;
+		this.background = null;
+		this.platforms = null;
+		this.bullets = null;
+		this.enemies = null;
+		this.firerate = 100;
+		this.nextFire = 0;
+	}
+
+	Game.prototype = {
+
+		create: function () {
+			// Set physics
+			this.physics.startSystem(Phaser.Physics.ARCADE);
+
+			// Background
+			this.background = this.add.sprite(0, 0, 'white');
+			this.background.scale.setTo(this.world.width, this.world.height);
+
+			// Platforms group
+			this.platforms = this.add.group();
+			this.platforms.enableBody = true;
+
+			// The ground
+			var ground = this.platforms.create(0, this.world.height - 10, 'black');
+			ground.scale.setTo(64, 1);
+			ground.body.immovable = true;
+
+			// Some ledges to jump on
+			var ledge = this.platforms.create(400, 250, 'black');
+			ledge.scale.setTo(10, 1);
+			ledge.body.immovable = true;
+
+			ledge = this.platforms.create(0, 300, 'black');
+			ledge.scale.setTo(10, 1);
+			ledge.body.immovable = true;
+
+			// Create the player
+			this.player = this.add.sprite(420, 0, 'player');
+			this.player.scale.setTo(0.25, 0.25);
+			this.game.physics.arcade.enable(this.player);
+
+			this.player.body.bounce.y = 0.2;
+			this.player.body.gravity.y = 500;
+			this.player.body.collideWorldBounds = true;
+
+			// Bullets
+			this.bullets = this.add.group();
+			this.bullets.enableBody = true;
+			this.bullets.createMultiple(50, 'black');
+			this.bullets.setAll('checkWorldBounds', true);
+			this.bullets.setAll('outOfBoundsKill', true);
+
+			// Enemies
+			this.enemies = this.add.group();
+			this.enemies.enableBody = true;
+
+			var enemy = this.enemies.create(20, 275, 'player');
+			enemy.scale.setTo(0.2, 0.2);
+			enemy.health = 100;
+			enemy.body.immovable = true;
+
+			enemy = this.enemies.create(200, 445, 'player');
+			enemy.scale.setTo(0.2, 0.2);
+			enemy.health = 100;
+			enemy.body.immovable = true;
+
+
+
+		},
+
+    update: function () {
+
+      // Move the player
+      this.movePlayer();
+
+      // Shoot!
+      if (this.input.activePointer.isDown) {
+      	this.shoot();
+      }
+
+      // Kill bullets when they hit platforms
+      this.physics.arcade.overlap(this.bullets, this.platforms, function (bullet, platform) {
+      	bullet.kill();
+      }, null, null);
+
+      // Kill enemies when show
+      this.physics.arcade.overlap(this.bullets, this.enemies, this.enemyShot, null, null);
+
+    },
+
+    movePlayer: function () {
+
+      var leftKey = Phaser.Keyboard.A,
+          rightKey = Phaser.Keyboard.D,
+          upKey = Phaser.Keyboard.W;
+
+      // Collide with platforms
+      this.physics.arcade.collide(this.player, this.platforms);
+
+      // Collied with enemies
+      this.physics.arcade.collide(this.player, this.enemies);
+
+      // Reset velocity
+      this.player.body.velocity.x = 0;
+
+      // Move left
+      if (this.input.keyboard.isDown(leftKey)) {
+        this.player.body.velocity.x = -150;
+      }
+      // Move right
+      else if (this.input.keyboard.isDown(rightKey)) {
+      	this.player.body.velocity.x = 150;
+      }
+
+      // Jump!
+      // console.log(this.player.body.touching.down);
+      if (this.input.keyboard.isDown(upKey) && this.player.body.touching.down) {
+      	this.player.body.velocity.y = -550;
+      }
+
+    },
+
+    shoot: function () {
+
+      var bullet;
+
+      // If shooting is allowed, SHOOT!
+      if (this.time.now > this.nextFire && this.bullets.countDead() > 0) {
+        this.nextFire = this.time.now + this.firerate;
+
+        bullet = this.bullets.getFirstDead();
+        bullet.reset(this.player.x, this.player.y);
+        this.physics.arcade.moveToPointer(bullet, 300);
+      }
+
+    },
+
+    enemyShot: function (bullet, enemy) {
+
+      // Decrease health
+      enemy.health -= 20;
+
+      // Kill the bullet
+      bullet.kill();
+
+      // If there's no health left, kill the enemy
+      if (enemy.health <= 0) {
+        enemy.kill();
+      }
+
+    }
+
+  };
+
+  window['blockz'] = window['blockz'] || {};
+  window['blockz'].Game = Game;
+
+}());
---
+++ src/js/menu.js
@@ -0,0 +1,45 @@
+(function() {
+
+	'use strict';
+
+	function Menu() {
+		this.titleTxt = null;
+		this.startTxt = null;
+	}
+
+  Menu.prototype = {
+
+    create: function () {
+
+      var x = this.game.width / 2,
+          y = this.game.height / 2;
+
+      // Title text
+      this.titleTxt = this.add.bitmapText(x, y, 'minecraftia', 'BLOCKZ!!1' );
+      this.titleTxt.align = 'center';
+      this.titleTxt.x = this.game.width / 2 - this.titleTxt.textWidth / 2;
+
+      // Start button
+      y = y + this.titleTxt.height + 5;
+      this.startTxt = this.add.bitmapText(x, y, 'minecraftia', 'JOIN');
+      this.startTxt.align = 'center';
+      this.startTxt.x = this.game.width / 2 - this.startTxt.textWidth / 2;
+
+      // Add click event to start the game
+      this.input.onDown.add(this.onDown, this);
+    },
+
+    update: function () {
+
+    },
+
+    onDown: function () {
+      // It's a GO!
+      this.game.state.start('game');
+    }
+  };
+
+  window['blockz'] = window['blockz'] || {};
+  window['blockz'].Menu = Menu;
+
+}());
